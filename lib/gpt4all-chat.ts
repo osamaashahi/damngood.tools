1d61a97a2203d37b3388f24f5b7dd1385d281de8
Dmytro Krasun <krasun.net@gmail.com>
2023-05-14 00:22:57 +0300

    Add simple command to test GPT4All

--- lib/gpt4all-chat.ts
+++ lib/gpt4all-chat.ts
@@ -2,9 +2,8 @@ import initializer from "./gpt4all"
 
 export async function prompt(prompt: string) {
     const gpt4all = await initializer.initialize()
-
     const resultText = await gpt4all.prompt(prompt)
-    
+
     return {
         text: resultText,
     }
