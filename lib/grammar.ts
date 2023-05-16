02a9e5723ce3bd5157e77f1d989c170ab9615373
Dmytro Krasun <krasun.net@gmail.com>
2023-05-16 21:25:50 +0300

    Improve the grammar fixing prompt

--- lib/grammar.ts
+++ lib/grammar.ts
@@ -1,7 +1,7 @@
 import { askOpenAIApi } from "./openai"
 
-export async function fixGrammar(text: string) {
-    const prompt = `Fix grammar and typos without changing meaning and wording in: [${text}]. Return the text without any instructions, just the fixed text:`
+export async function fixGrammar(text: string) {    
+    const prompt = `I want you to act as a grammar fixer, spelling corrector and improver. I will speak to you in any language and you will detect the language, and answer in the corrected and improved version of my text. I want you to replace only invalid words and sentences with fixed versions and without typos. Keep the meaning same. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My text: ${text}`
 
     return askOpenAIApi(prompt)
 }
