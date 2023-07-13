c44f028373f7ee2f698b1b9a262bf0c3308e8898
Dmytro Krasun <krasun.net@gmail.com>
2023-07-13 18:39:53 +0300

    Improve /thanks page

--- src/app/(app)/thanks/page.tsx
+++ src/app/(app)/thanks/page.tsx
@@ -5,11 +5,12 @@ export default function ThankYouPage() {
         <>
             <PageHeader
                 heading="Thank You"
-                subheading="Thank you for supporting the product by buying premium access."
+                subheading="Thank you for supporting the product by buying premium access. It usually takes a few minutes to receive your payment and upgrade your access."
             />
             <div className="max-w-[500px]">
-                In case you want to chat or have any questions, please don&rsquo;t
-                hesitate to write to <strong>support@damngood.tools</strong>.
+                In case you want to chat or have any questions, please
+                don&rsquo;t hesitate to write to{" "}
+                <strong>support@damngood.tools</strong>.
             </div>
         </>
     )
